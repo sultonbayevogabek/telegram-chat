9d2c739b28207f02013f5049357cc16bf12fc6bc
sultonbayevogabek <sultonbayevogabek@gmail.com>
2021-07-27 22:43:40 +0500

    oxirigacha bitdi

--- public/scripts/_zoom.js
+++ public/scripts/_zoom.js
@@ -3,7 +3,7 @@ import { selectOne } from './_functions'
 export default function () {
    try {
       if (document.body.clientWidth < 1400 && !window.location.href.includes('/auth')) {
-         document.body.style.zoom = 0.7
+         document.body.style.zoom = 0.8
          selectOne('.app').style.height = document.body.clientHeight * 1.43 + 'px'
       }
    } catch (e) {}
